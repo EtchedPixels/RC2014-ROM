8005d5049ed0b9101cd0d404de245f8510c0f4ff
Alan Cox <alan@linux.intel.com>
2019-10-18 21:55:07 +0100

    loader: an initial cut at a 6303 bootstrap loader

---
+++ 6303/loader.s
@@ -0,0 +1,131 @@
+;
+;	We are loaded at $0200.
+;	0000-7FFF are RAM 8000-FFFF ROM (except I/O)
+;
+;	We load an image from 0400-FDFF
+;	and then jump to C102 if the marker is right (this needs to move
+;	now the I/O has moved)
+;
+
+	ORG $0200
+
+	FCB $63
+	FCB $03
+start:
+	; Map the full 64K to RAM
+	LDAA #34
+	STAA $FE7A
+	LDAA #35
+	STAA $FE7B
+	; Our map is now 32 33 34 35
+
+	LDX #$0400
+
+	LDAA #$01	; 0 is the partition/boot block
+	STAA sector
+
+	BSR waitready
+
+	LDAA #$E0
+	STAA $FE16	; Make sure we are in LBA mode
+dread:
+	BSR waitready
+	LDAA #'.'
+	BSR outchar
+	LDAA sector
+	CMPA #$7D	; loaded all of the image ?
+	BEQ load_done
+	INC sector
+	STAA $FE13
+	LDAA #$01
+	STAA $FE12	; num sectors (drives may clear this each I/O)
+	BSR waitready
+	LDAA #$20
+	STAA $FE17	; read command
+
+	BSR waitdrq
+
+	CLRB
+bytes:
+	LDAA $FE10
+	STAA ,X
+	INX
+	LDAA $FE10
+	STAA ,X
+	INX
+	DECB
+	BNE bytes2
+	BRA dread
+
+load_done:
+	LDD $C000	; FIXME: move this
+	CMPA #$03
+	BNE bad_load
+	CMPB #$63
+	BNE bad_load
+
+	LDX #running
+	BSR outstring
+	JMP $C002	; FIXME: move this
+
+bad_load:
+	LDX #badimg
+	BSR outstring
+stop:
+	BRA stop
+
+waitready:
+	LDAA $FE17
+	ANDA #$40
+	BEQ waitready
+	RTS
+
+waitdrq:
+	LDAA $FE17
+	ANDA #$09
+	BEQ waitdrq
+	RORA
+	BCC wait_drq_done
+	LDAA $FE11
+	BSR outcharhex
+	BRA bad_load
+
+wait_drq_done:
+	RTS
+
+outstring:
+	LDAA ,X
+	BEQ outdone1
+	BSR outchar
+	INX
+	JMP outstringl
+
+outcharhex:
+	TAB
+	RORA
+	RORA
+	RORA
+	RORA
+	BSR outcharhex1
+	TBA
+outcharhex1:
+	ANDA #$0F
+	ADDA #$30
+	CMPA #$3A
+	BCC outchar
+	ADDA #7
+outchar:
+	PSHA
+outcharw:
+	LDAA $11	; FIXME: use TIM
+	ANDA #$20
+	BEQ outcharw
+	PULA
+	STAA $13
+outdone1:
+	RTS
+badimg:
+	FCB 13,10
+	FCC "Image not bootable."
+running:
+	FCB 13,10,0
