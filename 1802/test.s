f898383480d72d2d6839f5cd6c0bcfce10cd96ce
Alan Cox <alan@linux.intel.com>
2020-03-30 21:27:09 +0100

    test: test rom correct the I/O
    With this done it gives flashing lights

--- 1802/test.s
+++ 1802/test.s
@@ -2,10 +2,33 @@
 	.org	0
 
 start:
-	ldi	#0xFE
+	ldi	#0xFF
 	phi	6
 	ldi	#0x80
 	plo	6
-	ldi	#0xAA
+	ldi	#0xFF
+	plo	8
+loop:
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	dec	7
+	glo	7
+	bnz	loop
+	dec	8
+	glo	8
 	str	6
-loop:	br	loop
+	br	loop
